d24a1c2050ed0bb80704c2a7b4fcc27d826c4dde
MarkoObrvan <obrvan.marko@gmail.com>
2015-05-08 11:36:25 +0200

    added new ideas for implementation

--- src/DetectionModule.cpp
+++ src/DetectionModule.cpp
@@ -5,6 +5,8 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+
 
 #include <cv_bridge/cv_bridge.h>
 #include "stereo_msgs/DisparityImage.h"
@@ -22,6 +24,10 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <tf/transform_broadcaster.h>
 #include <pcl/io/pcd_io.h>
+#include <pcl/common/common_headers.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/visualization/cloud_viewer.h>
+
 
 #include <iostream>
 
@@ -32,8 +38,13 @@
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
+pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
+
 using namespace sensor_msgs;
 using namespace message_filters;
+using namespace cv;
+
+RNG rng(12345);
 
 static unsigned char colormap[768] =
 { 150, 150, 150,
@@ -294,10 +305,53 @@ static unsigned char colormap[768] =
 255, 0, 0,
 };
 
+
 void disparityImageCallback(const stereo_msgs::DisparityImageConstPtr& disparity_msg, const sensor_msgs::ImageConstPtr& left_msg)
 {
 	
-	std::cout <<"In callback"<<std::endl;
+	/*
+	cv::Mat camera_matrix1 = (Mat_<float>(3,3) <<1033.842144566857, 0, 641.7568472235299, 0, 1033.9910623563098, 477.77802466524605, 0, 0, 1);
+	cv::Mat camera_matrix2 = (Mat_<float>(3,3) <<1036.1905066339948, 0, 658.0591592476255, 0, 1036.2230336709486, 467.8432113246438, 0, 0, 1);
+	cv::Mat dist_coeffs1 = (Mat_<float>(1,5) <<-0.34760090909684316, 0.13893127178322365, 0.0002624157294778045, 0.00017474106031351618, 0);
+	cv::Mat dist_coeffs2 = (Mat_<double>(1,5) <<-0.34299615135008976, 0.13066865403875236, 0.0005705026352960487, -0.0002575017480093701, 0);
+	cv::Mat R = (Mat_<double>(3,3) <<0.9999985657944156, 0.0008153120094810814, -0.0014844781692219457, -0.0008129287740475245, 0.999998381025527, 0.0016053327159822335,0.0014857846129322403, -0.0016041236385868848, 0.999997609612859);
+	cv::Mat T = (Mat_<double>(3,1) <<-0.2394236026388935, 0.00027744418645254785, 7.347465192346994e-05);
+	*/
+	
+	/*
+	cv::Mat_<double> cameraMatrix1(3,3); // 3x3 matrix
+	cv::Mat_<double> distCoeffs1(5,1);   // 5x1 matrix for five distortion coefficients
+	cv::Mat_<double> cameraMatrix2(3,3); // 3x3 matrix
+	cv::Mat_<double> distCoeffs2(5,1);   // 5x1 matrix
+	cv::Mat_<double> R(3,3);             // 3x3 matrix, rotation left to right camera
+	cv::Mat_<double> T(3,1);             // * 3 * x1 matrix, translation left to right proj. center
+	
+	cameraMatrix1 << 1033.842144566857, 0, 641.7568472235299, 0, 1033.9910623563098, 477.77802466524605, 0, 0, 1;
+	cameraMatrix2 << 1036.1905066339948, 0, 658.0591592476255, 0, 1036.2230336709486, 467.8432113246438, 0, 0, 1;
+	distCoeffs1   << -0.34760090909684316, 0.13893127178322365, 0.0002624157294778045, 0.00017474106031351618, 0;
+	distCoeffs2   << -0.34299615135008976, 0.13066865403875236, 0.0005705026352960487, -0.0002575017480093701, 0;
+	R << 0.9999985657944156, 0.0008153120094810814, -0.0014844781692219457,
+	-0.0008129287740475245, 0.999998381025527, 0.0016053327159822335,0.0014857846129322403, -0.0016041236385868848, 0.999997609612859;
+	T << -0.2394236026388935, 0.00027744418645254785, 7.347465192346994e-05;
+
+	cv::Mat R1,R2,P1,P2,Q;
+	
+	cv::stereoRectify(cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, cv::Size(1280, 960), R, T, R1, R2, P1, P2, Q);	
+	
+	std::cout<< "Q" << Q << std::endl;
+	std::cout<< "R" << R << std::endl;
+	std::cout<< "T" << T << std::endl;
+	std::cout<< "R1" << R1 << std::endl;
+	std::cout<< "R2" << R2 << std::endl;
+	std::cout<< "P1" << P1 << std::endl;
+	std::cout<< "P2" << P2 << std::endl;
+	*/
+	
+	
+	
+	int CONTUR_LENGTH_THRESHOLD = 60;
+	
+	//std::cout <<"In callback"<<std::endl;
 	
 	ros::NodeHandle nh;
 	ros::Publisher publisher = nh.advertise<sensor_msgs::PointCloud2> ("FromDisparityToWorld", 10);
@@ -311,168 +365,135 @@ void disparityImageCallback(const stereo_msgs::DisparityImageConstPtr& disparity
 	
 	last_image_ = cv_bridge::toCvCopy(left_msg, sensor_msgs::image_encodings::MONO8);
 	
-    cv::Mat edge, draw;
- 
-    Canny( last_image_->image, edge, 60, 140, 3);
- 
+	cv::Mat edge, edge2, draw;
+	
+	
+	const clock_t begin_time = clock();
+	std::cout << "Timestart! " <<float( clock ())/  CLOCKS_PER_SEC<< "  ---";
+	
+	GaussianBlur(last_image_->image, edge2, cv::Size(7,7), 4, 4, 0);
+	//bilateralFilter(last_image_->image, edge2,-1, 50, 7);
+	
+	
+	//Canny( last_image_->image, edge, 60, 120, 3);
+	Canny( edge2, edge, 60, 120, 3);
+
 	
 	
 	
-    edge.convertTo(draw, CV_8U);
-    cv::imshow("CannyEdge", draw);
+	edge.convertTo(draw, CV_8U);
+	cv::imshow("CannyEdge", draw);
+	
+	std::cout << float( clock () - begin_time ) / (double) CLOCKS_PER_SEC<<"    ";
 	
 	
-	try
+	
+	
+	
+	
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	
+	  /// Find contours
+	findContours( edge, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) ); //CV_RETR_EXTERNAL  CV_RETR_TREE
+	
+	//Remove small conturs
+	
+	/*
+	to slow, have to work way around this
+	for (vector<vector<Point> >::iterator it = contours.begin(); it!=contours.end(); )
 	{
+   		if (it->size()<CONTUR_LENGTH_THRESHOLD)
+        	it=contours.erase(it);
+    	else
+        	++it;
+	}*/
+	
+	
+	/* also to slow
+	vector<Moments> ContArea(contours.size());
+	for( int i = 0; i < contours.size(); i++ )
+    {  
+        ContArea[i] = moments(contours[i], false);
+    }
+	
+	int iterMoment=0;
+	for (vector<vector<Point> >::iterator it = contours.begin(); it!=contours.end(); )
+	{
+   		if (ContArea[iterMoment].m00 < 100)
+        	it=contours.erase(it);
+    	else
+        	++it;
 		
-		float min_disparity = disparity_msg->min_disparity;
-		float max_disparity = disparity_msg->max_disparity;
-		float multiplier = 255.0f / (max_disparity - min_disparity);
+		iterMoment++;
+	}
+	*/
+	
+	  /// Approximate contours to polygons + get bounding rects and circles
+  vector<vector<Point> > contours_poly( contours.size() );
+  vector<Rect> boundRect( contours.size() );
+  vector<Point2f>center( contours.size() );
+  vector<float>radius( contours.size() );
 
-		std::cout<< "minimal disparity: " << min_disparity << "  maximal disparity: " << max_disparity << std::endl;
-		std::cout<< "focal length pixels: " << disparity_msg->f << "  focal baseline world units: " << disparity_msg->T << std::endl;
+  for( int i = 0; i < contours.size(); i++ )
+     { 
+	  approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
+       boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+       //minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
+     }
+	
+	/*
+	int iterMoment=0;
+	for (vector<vector<Point> >::iterator it = contours.begin(); it!=contours.end(); )
+	{
+   		if (boundRect[iterMoment].size() < 100)
+        	it=contours.erase(it);
+    	else
+        	++it;
 		
-		cv::Mat_<cv::Vec3b> disparity_color_;
-		disparity_color_.create(disparity_msg->image.height, disparity_msg->image.width);
-                
-        cv::Mat_<cv::Vec3b> edges_;
-		edges_.create(disparity_msg->image.height, disparity_msg->image.width);
-		
-		//constructs a matrix on top of user-allocated data. step is in bytes(!!!), regardless of the type 
-		const cv::Mat_<float> dmat(disparity_msg->image.height, disparity_msg->image.width,
-		(float*)&disparity_msg->image.data[0], disparity_msg->image.step);
-		
-		typedef boost::multi_array<float, 3> array_type;
-		typedef array_type::index index;
-		array_type mat3DfromDisparity(boost::extents[disparity_msg->image.height][disparity_msg->image.width][3]);
-		
-		int centerCol = disparity_msg->image.width/2;
-		int centerRow = disparity_msg->image.height/2;
-		int u,v;
-		
-		/*
-                Run trough disparity map and calculate depth map
-		*/
-		for (int row = 0; row < disparity_msg->image.height; ++row) {
-			
-			if(row== disparity_msg->image.height-1) std::cout<< "Run this many times:P : "<< g+1 << std::endl;
-			
-			const float* d = dmat[row];
-			
-			for (int col = 0; col < disparity_msg->image.width; ++col) {
+		iterMoment++;
+	}
+	*/
 
-				if(d[col]!=-1){
+	/// Draw contours
+	Mat drawing = Mat::zeros( edge.size(), CV_8UC3 );
+	for( int i = 0; i< contours.size(); i++ )
+	{
+		Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+		drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
+		rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+	}
+
+	/// Show in a window
+
+	imshow( "Contours", drawing );
+	
+	
+	
+	
+	//Disparity calculation and parametars
+	
+	float min_disparity = disparity_msg->min_disparity;
+	float max_disparity = disparity_msg->max_disparity;
+	float multiplier = 255.0f / (max_disparity - min_disparity);
+	
+	cv::Mat_<cv::Vec3b> disparity_color_;
+	disparity_color_.create(disparity_msg->image.height, disparity_msg->image.width);
+	
+	//constructs a matrix on top of user-allocated data. step is in bytes(!!!), regardless of the type 
+	const cv::Mat_<float> dmat(disparity_msg->image.height, disparity_msg->image.width, (float*)&disparity_msg->image.data[0], disparity_msg->image.step);
+	
+	cv::Mat dispar(disparity_msg->image.height, disparity_msg->image.width,CV_8U);
+	
+	for (int row = 0; row < disparity_color_.rows; ++row) {
+		const float* d = dmat[row];
+		for (int col = 0; col < disparity_color_.cols; ++col) {
 				
-//std::cout<<d[col]<<" "<<std::endl;
-					
-					float Z = disparity_msg->f * disparity_msg->T / d[col];
-					
-					u = col - centerCol;
-					v = row - centerRow;
-					
-					mat3DfromDisparity[row][col][0]= Z; //Z
-					mat3DfromDisparity[row][col][1]= u * Z/disparity_msg->f;   //X = uZ/f
-					mat3DfromDisparity[row][col][2]= v * Z/disparity_msg->f;   //Y=vZ/f
-
-					msg->points.push_back (pcl::PointXYZ(Z, mat3DfromDisparity[row][col][1], mat3DfromDisparity[row][col][2]));
-				}
-			}
-		}
-		
-                /*
-                 Publish in ROS point cloud (and kill my computer -.-)
-                 */		
-		msg->height = 0;
-		msg->width = 0;
-		msg->header.stamp =  g;
-		pcl::toROSMsg(*msg, output);	
-		g++;
-		publisher.publish (output);
-		msg->points.clear();
-		
-		/*
-			Run trough disparity map and calculate edges using: http://www.vision.caltech.edu/VisionWiki/images/9/92/Franke96fast.pdf
-			something like given in example. TODO mod it to calculate smooth changes in disparity, aka. sides of car where one side of disparity
-			is ~constant and other one is smooth decreasing using neighbourghs at distance z:
-			
-			modified matrix					
-						i
-						-
-						-							usual edge detector matrix
-						-								i
-		j	-	-	-	x	-	-	-	-	j		j	x	j
-						-								i
-						-
-						-
-						-
-						i
-						
-		usual edge detector g(x) - j>threshold ---> x is vertical edge and belongs to object (same position is disparity of object)
-							g(x) - i>threshold ----> x is horizontal edge and belongs to object
-			
-		*/
-		
-		
-		//unmodified matrix test
-		
-		for (int row = 1; row < disparity_msg->image.height - 1; ++row) {
-			
-			const float* up = dmat[row-1];
-			const float* mid = dmat[row];
-                        const float* bot = dmat[row+1];
-                        
-			for (int col = 1; col < disparity_msg->image.width - 1; ++col) {
-                            
-                            //check horizontal
-                            if(mid[col]-up[col]>THRESHOLD || mid[col]-bot[col]>THRESHOLD){
-                                edges_(row, col)[0] = 255;
-                                continue;
-                            }
-                            
-                            if(mid[col]-mid[col-1]>THRESHOLD || mid[col]-mid[col+1]>THRESHOLD){
-                                edges_(row, col)[1] = 255;
-                                continue;
-                            }
-                            
-                            edges_(row, col)[0] = edges_(row, col)[1] = edges_(row, col)[2] = 0;
-                            
-                        }
-                    }
-		
-                cv::imshow("Edge map", edges_);
-		
-		
-		/******THIS IS TEST FOR ADVANCE EDGE DETECTOR******/
-			for (int row = 1; row < disparity_msg->image.height - VERTICAL_MERGE_BOUNDARY ; ++row) {
-
-				for (int col = 1; col < disparity_msg->image.width - 1; ++col) {
-                            
-					for(int i=VERTICAL_MERGE_BOUNDARY; i>0; i--){
-						if (edges_(row-i, col)[1] == 255  && edges_(row, col)[1] == 255){
-							while(i!=0){
-								edges_(row-i, col)[2]=255;
-								i--;
-							}
-						}
-					}
-                            
-				}
-			}
-		
-         	cv::imshow("Edge map", edges_);
-		
-		
-		
-                
-                /* Tesrun, no disparity during this */
-		/*DISPLAY DISPARITY WITH COLOR MAP*/
-		for (int row = 0; row < disparity_color_.rows; ++row) {
-			const float* d = dmat[row];
-			for (int col = 0; col < disparity_color_.cols; ++col) {
-				
-				if(d[col]==-1){
-					disparity_color_(row, col)[2]=disparity_color_(row, col)[1]=disparity_color_(row, col)[0]=0;
-					
+			if(d[col]==-1){
+					disparity_color_(row, col)[2]=0;
+					disparity_color_(row, col)[1]=0;
+					disparity_color_(row, col)[0]=0;
+					dispar.at<uchar>(row, col, CV_8U) = 0;
 					continue;
 				}
 				
@@ -482,19 +503,23 @@ void disparityImageCallback(const stereo_msgs::DisparityImageConstPtr& disparity
 				disparity_color_(row, col)[2] = colormap[3*index + 0];
 				disparity_color_(row, col)[1] = colormap[3*index + 1];
 				disparity_color_(row, col)[0] = colormap[3*index + 2];
+				dispar.at<uchar>(row, col, CV_8U) = index;
 			}
 		}
-
-		cv::imshow("view", disparity_color_);
-                
-                
-		//std::cout<<msg->T<<endl;
-		//cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
-	}
-	catch (cv_bridge::Exception& e)
-	{
-	   // ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
-	}
+	
+	cv::imshow("disparity color", disparity_color_);
+	cv::imshow("view", dispar);
+	
+	cv::Mat disparityCanny;
+	Canny( dispar, disparityCanny, 20, 100, 3);
+	cv::imshow("DisparityEdge", disparityCanny);
+	
+	
+	
+	std::cout << "Whole function time lapse:   " <<float( clock () - begin_time ) / (double) CLOCKS_PER_SEC<<std::endl;
+	std::cout << " Timend! " <<float( clock ())/  CLOCKS_PER_SEC<<std::endl;
+	
+	
 }
 
 int main(int argc, char **argv){
@@ -503,21 +528,100 @@ int main(int argc, char **argv){
 		ros::NodeHandle nh;
 		
 		cv::namedWindow("view");
-		cv::namedWindow("Edge map");
+		cv::namedWindow("disparity color");
+		cv::namedWindow("DisparityEdge");
 		cv::namedWindow("CannyEdge");
+		cv::namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
 		cv::startWindowThread();
 		
 		message_filters::Subscriber<stereo_msgs::DisparityImage> disp_sub(nh, "/camera/stereo_camera_LR/disparity", 1);
  		message_filters::Subscriber<Image> left_sub(nh, "/camera/stereo_camera_LR/left/image_raw", 1);
 		
-		typedef sync_policies::ApproximateTime<stereo_msgs::DisparityImage, Image> MySyncPolicy; //ovo dodao
-		Synchronizer<MySyncPolicy> sync(MySyncPolicy(20), disp_sub, left_sub); /// ovo dodao
-	
-		//TimeSynchronizer<stereo_msgs::DisparityImage, Image> sync(disp_sub, left_sub, 10);  //ovo maknuo
+		typedef sync_policies::ApproximateTime<stereo_msgs::DisparityImage, Image> MySyncPolicy; 
+		Synchronizer<MySyncPolicy> sync(MySyncPolicy(20), disp_sub, left_sub); 
 		
 		sync.registerCallback(boost::bind(&disparityImageCallback, _1, _2));
 		
 		ros::spin();
 		cv::destroyWindow("view");
+		cv::destroyWindow("disparity color");
+		cv::destroyWindow("DisparityEdge");
+		cv::destroyWindow("CannyEdge");
+		cv::destroyWindow("Contours");
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+	/*
+	
+	TEST part START
+	
+	*/
+	
+/*
+  //Create matrix that will contain 3D corrdinates of each pixel
+  cv::Mat recons3D(dispar.size(), CV_32FC3);
+  
+  //Reproject image to 3D
+  std::cout << "Reprojecting image to 3D..." << std::endl;
+  cv::reprojectImageTo3D( dispar, recons3D, Q, false, CV_32F );
+	
+	
+
+  //Create point cloud and fill it
+  std::cout << "Creating Point Cloud..." <<std::endl;
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr point_cloud_ptr (new pcl::PointCloud<pcl::PointXYZRGB>);
+  
+  float px, py, pz;
+  uchar pr, pg, pb;
+  
+  for (int i = 0; i < recons3D.rows; i++)
+  {
+
+    float* recons_ptr = recons3D.ptr<float>(i);
+
+    for (int j = 0; j < recons3D.cols; j++)
+    {
+      //Get 3D coordinates
+
+      px = (float)recons_ptr[3*j];
+      py = (float)recons_ptr[3*j+1];
+      pz = (float)recons_ptr[3*j+2];
+
+     // std::cout<<"3d: "<<px<<"  "<<py<<"  "<<pz<<recons3D<<std::endl;
+      
+      //Insert info into point cloud structure
+      pcl::PointXYZRGB point;
+      point.x = px;
+      point.y = py;
+      point.z = pz;
+	  point.rgb = 21921;
+
+      point_cloud_ptr->points.push_back (point);
+    }
+  }
+  point_cloud_ptr->width = (int) point_cloud_ptr->points.size();
+  point_cloud_ptr->height = 1;
+  
+  //Create visualizer
+	//boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
+
+	
+	viewer.showCloud (point_cloud_ptr);
+*/
+		/*
+	
+	TEST part END
+	
+	*/
+	
